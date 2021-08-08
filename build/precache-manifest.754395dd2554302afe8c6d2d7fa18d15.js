99e73be5acdcd50bb10f94a081a4e00d888df1c6
Nitish <genitish@gmail.com>
2021-08-09 01:58:21 +0530

    deploy

---
+++ build/precache-manifest.754395dd2554302afe8c6d2d7fa18d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c7e77b7d3cca8eeb8a426a45ff478b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b387d14fc9c6633d95c6",
+    "url": "/static/css/main.92e611a1.chunk.css"
+  },
+  {
+    "revision": "2b0678c0a5093a00d351",
+    "url": "/static/js/2.49f7cfb7.chunk.js"
+  },
+  {
+    "revision": "b387d14fc9c6633d95c6",
+    "url": "/static/js/main.abd131ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
